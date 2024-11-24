d90e7c03e044a171aca794b5861e8422854cb680
Kiemtien <45493549+vietnamai@users.noreply.github.com>
2024-11-25 01:39:04 +0700

    Update telegram.js

--- js/telegram.js
+++ js/telegram.js
@@ -22,37 +22,17 @@ function initTelegram() {
     const username = user.username;
     const avatar = user.photo_url;  // URL của avatar người dùng
 
-    // Lưu thông tin người dùng vào Firebase (giả sử sử dụng Firebase SDK)
+    // Lưu thông tin người dùng vào Firebase
     saveUserInfoToFirebase(userId, firstName, lastName, username, avatar);
 
     // Gửi dữ liệu đến bot Telegram (nếu cần thiết)
     // Ví dụ: gửi "balance" khi người dùng nhấn nút
     tg.MainButton.onClick(() => {
-        // Bạn có thể thực hiện các hành động sau khi người dùng nhấn nút
         console.log("Balance check clicked!");
     });
 
     tg.MainButton.text = "Check Balance";
     tg.MainButton.show();
-}
-
-// Hàm lưu thông tin người dùng vào Firebase
-function saveUserInfoToFirebase(userId, firstName, lastName, username, avatar) {
-    // Thực hiện lưu thông tin người dùng vào Firebase
-    const db = firebase.database();
-    const userRef = db.ref('users/' + userId);
-
-    userRef.set({
-        first_name: firstName,
-        last_name: lastName,
-        username: username,
-        avatar: avatar,
-        balance: 0,  // Khởi tạo balance ban đầu cho người dùng
-    }).then(() => {
-        console.log(`User ${firstName} ${lastName} saved to Firebase.`);
-    }).catch((error) => {
-        console.error("Error saving user info to Firebase:", error);
-    });
 }
 
 // Gọi hàm khởi tạo Telegram Web App
