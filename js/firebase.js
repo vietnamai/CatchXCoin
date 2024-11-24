d6c94ea4aeaf8f9e14d66efd75f2b9b5ee5df813
Kiemtien <45493549+vietnamai@users.noreply.github.com>
2024-11-25 02:03:47 +0700

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,9 +1,24 @@
 // File: firebase.js
 
+// Cấu hình Firebase (API key, Project ID, ... được cung cấp trong Firebase Console)
+const firebaseConfig = {
+  apiKey: "AIzaSyCOHdI4tVObleAkiuIUymMBNEz3OPue-7Y",
+  authDomain: "catchxcoin.firebaseapp.com",
+  projectId: "catchxcoin",
+  storageBucket: "catchxcoin.firebasestorage.app",
+  messagingSenderId: "1044694743228",
+  appId: "1:1044694743228:web:a50528274809fc880178b9",
+  measurementId: "G-3HL2PJ5SRD"
+};
+
+// Khởi tạo Firebase
+const app = firebase.initializeApp(firebaseConfig);  // Khởi tạo Firebase với cấu hình của bạn
+const db = firebase.database(); // Kết nối tới Realtime Database
+const analytics = firebase.analytics(); // Kết nối tới Firebase Analytics
+
 // Lưu thông tin người dùng vào Firebase
 function saveUserInfoToFirebase(userId, firstName, lastName, username, avatar) {
-    const db = firebase.database(); // Sử dụng Realtime Database của Firebase
-    const userRef = db.ref('users/' + userId);
+    const userRef = db.ref('users/' + userId); // Tạo tham chiếu tới node "users" trong Realtime Database
 
     userRef.set({
         first_name: firstName,
