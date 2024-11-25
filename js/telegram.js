f6ff9cc693a85d62baf558b87b5113dbb2bcf6a4
Kiemtien <45493549+vietnamai@users.noreply.github.com>
2024-11-25 12:32:22 +0700

    Update telegram.js

--- js/telegram.js
+++ js/telegram.js
@@ -16,7 +16,7 @@ function initTelegram() {
         // Kiểm tra và log thông tin người dùng
         const user = tg.initDataUnsafe.user;
         console.log("User Info:", user);
-        alert(user)
+        alert(user.last_name)
         const userId = user.id;
         const firstName = user.first_name;
         const lastName = user.last_name;
@@ -26,13 +26,6 @@ function initTelegram() {
         // Lưu thông tin người dùng vào Firebase
         saveUserInfoToFirebase(userId, firstName, lastName, username, avatar);
 
-        // Đặt các thông tin và hành động cho nút chính của Telegram Web App
-        tg.MainButton.onClick(() => {
-            console.log("Balance check clicked!");
-        });
-
-        tg.MainButton.text = "Check Balance";
-        tg.MainButton.show();
     } catch (error) {
         alert("error 2")
         console.error("Error during Telegram WebApp initialization:", error);
