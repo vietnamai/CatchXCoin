28b341d78f12b09f939894b3909b855d12009594
Kiemtien <45493549+vietnamai@users.noreply.github.com>
2024-11-26 09:58:37 +0700

    Update js

---
+++ js/R_Cannon.js
@@ -0,0 +1,81 @@
+// File: js/R_Cannon.js
+
+const cannonTypes = [
+    {
+        image: "cannon1.png",
+        power: 1,
+        frames: [
+            { rect: [0, 0, 74, 74], label: "default" },
+            { rect: [74, 0, 74, 74] },
+            { rect: [148, 0, 74, 74] },
+            { rect: [222, 0, 74, 74], jump: "default" }
+        ],
+        bullet: "bullet1.png"
+    },
+    {
+        image: "cannon2.png",
+        power: 2,
+        frames: [
+            { rect: [0, 0, 74, 76], label: "default" },
+            { rect: [74, 0, 74, 76] },
+            { rect: [148, 0, 74, 76] },
+            { rect: [222, 0, 74, 76], jump: "default" }
+        ],
+        bullet: "bullet2.png"
+    },
+    {
+        image: "cannon3.png",
+        power: 3,
+        frames: [
+            { rect: [0, 0, 74, 76], label: "default" },
+            { rect: [74, 0, 74, 76] },
+            { rect: [148, 0, 74, 76] },
+            { rect: [222, 0, 74, 76], jump: "default" }
+        ],
+        bullet: "bullet3.png"
+    },
+    {
+        image: "cannon4.png",
+        power: 4,
+        frames: [
+            { rect: [0, 0, 74, 83], label: "default" },
+            { rect: [74, 0, 74, 83] },
+            { rect: [148, 0, 74, 83] },
+            { rect: [222, 0, 74, 83], jump: "default" }
+        ],
+        bullet: "bullet4.png"
+    },
+    {
+        image: "cannon5.png",
+        power: 5,
+        frames: [
+            { rect: [0, 0, 74, 85], label: "default" },
+            { rect: [74, 0, 74, 85] },
+            { rect: [148, 0, 74, 85] },
+            { rect: [222, 0, 74, 85], jump: "default" }
+        ],
+        bullet: "bullet5.png"
+    },
+    {
+        image: "cannon6.png",
+        power: 6,
+        frames: [
+            { rect: [0, 0, 74, 90], label: "default" },
+            { rect: [74, 0, 74, 90] },
+            { rect: [148, 0, 74, 90] },
+            { rect: [222, 0, 74, 90], jump: "default" }
+        ],
+        bullet: "bullet6.png"
+    },
+    {
+        image: "cannon7.png",
+        power: 7,
+        frames: [
+            { rect: [0, 0, 74, 94], label: "default" },
+            { rect: [74, 0, 74, 94] },
+            { rect: [148, 0, 74, 94] },
+            { rect: [222, 0, 74, 94], jump: "default" }
+        ],
+        bullet: "bullet7.png"
+    }
+];
