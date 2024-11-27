4dc7e57bc7beaa66d8f051e781630bd74783d056
Kiemtien <45493549+vietnamai@users.noreply.github.com>
2024-11-27 08:12:32 +0700

    Update R_Cannon.js

--- js/R_Cannon.js
+++ js/R_Cannon.js
@@ -3,25 +3,79 @@
 export const cannonTypes = [
     {
         image: "cannon1.png",
-        power: 1,
         frames: [
-            { rect: [0, 0, 74, 74], label: "default" },
-            { rect: [74, 0, 74, 74] },
-            { rect: [148, 0, 74, 74] },
-            { rect: [222, 0, 74, 74], jump: "default" }
+            { rect: [0, 0, 74, 74] },
+            { rect: [0, 74, 74, 74] },
+            { rect: [0, 148, 74, 74] },
+            { rect: [0, 222, 74, 74] },
+            { rect: [0, 296, 74, 74], stop: 1 }
         ],
-        bullet: "bullet1.png"
+        mixin: { regX: 37, regY: 45, useFrames: true, interval: 3, power: 1 }
     },
     {
         image: "cannon2.png",
-        power: 2,
         frames: [
-            { rect: [0, 0, 74, 76], label: "default" },
-            { rect: [74, 0, 74, 76] },
-            { rect: [148, 0, 74, 76] },
-            { rect: [222, 0, 74, 76], jump: "default" }
+            { rect: [0, 0, 74, 76] },
+            { rect: [0, 76, 74, 76] },
+            { rect: [0, 152, 74, 76] },
+            { rect: [0, 228, 74, 76] },
+            { rect: [0, 304, 74, 76], stop: 1 }
         ],
-        bullet: "bullet2.png"
+        mixin: { regX: 37, regY: 45, useFrames: true, interval: 3, power: 2 }
     },
-    // Các súng khác...
+    {
+        image: "cannon3.png",
+        frames: [
+            { rect: [0, 0, 74, 76] },
+            { rect: [0, 76, 74, 76] },
+            { rect: [0, 152, 74, 76] },
+            { rect: [0, 228, 74, 76] },
+            { rect: [0, 304, 74, 76], stop: 1 }
+        ],
+        mixin: { regX: 37, regY: 45, useFrames: true, interval: 3, power: 3 }
+    },
+    {
+        image: "cannon4.png",
+        frames: [
+            { rect: [0, 0, 74, 83] },
+            { rect: [0, 83, 74, 83] },
+            { rect: [0, 166, 74, 83] },
+            { rect: [0, 249, 74, 83] },
+            { rect: [0, 332, 74, 83], stop: 1 }
+        ],
+        mixin: { regX: 37, regY: 45, useFrames: true, interval: 3, power: 4 }
+    },
+    {
+        image: "cannon5.png",
+        frames: [
+            { rect: [0, 0, 74, 85] },
+            { rect: [0, 85, 74, 85] },
+            { rect: [0, 170, 74, 85] },
+            { rect: [0, 255, 74, 85] },
+            { rect: [0, 340, 74, 85], stop: 1 }
+        ],
+        mixin: { regX: 37, regY: 45, useFrames: true, interval: 3, power: 5 }
+    },
+    {
+        image: "cannon6.png",
+        frames: [
+            { rect: [0, 0, 74, 90] },
+            { rect: [0, 90, 74, 90] },
+            { rect: [0, 180, 74, 90] },
+            { rect: [0, 270, 74, 90] },
+            { rect: [0, 360, 74, 90], stop: 1 }
+        ],
+        mixin: { regX: 37, regY: 45, useFrames: true, interval: 3, power: 6 }
+    },
+    {
+        image: "cannon7.png",
+        frames: [
+            { rect: [0, 0, 74, 94] },
+            { rect: [0, 94, 74, 94] },
+            { rect: [0, 188, 74, 94] },
+            { rect: [0, 282, 74, 94] },
+            { rect: [0, 376, 74, 94], stop: 1 }
+        ],
+        mixin: { regX: 37, regY: 45, useFrames: true, interval: 3, power: 7 }
+    }
 ];
