fd211c1f6437a78cb6bf9bfc55999eb341197f8c
Kiemtien <45493549+vietnamai@users.noreply.github.com>
2024-11-29 11:47:28 +0700

    Update js

---
+++ js/R_Web.js
@@ -0,0 +1,9 @@
+const webTypes = [
+	{image:"web.png", rect:[319,355,116,118], regX:58, regY:59, polyArea:[{x:20, y:20}, {x:100, y:20}, {x:100, y:100}, {x:20, y:100}]},
+	{image:"web.png", rect:[0,399,137,142], regX:68, regY:71, polyArea:[{x:20, y:20}, {x:120, y:20}, {x:120, y:120}, {x:20, y:120}]},
+	{image:"web.png", rect:[163,355,156,162], regX:78, regY:81, polyArea:[{x:20, y:20}, {x:140, y:20}, {x:140, y:140}, {x:20, y:140}]},
+	{image:"web.png", rect:[242,181,180,174], regX:90, regY:87, polyArea:[{x:20, y:20}, {x:160, y:20}, {x:160, y:160}, {x:20, y:160}]},
+	{image:"web.png", rect:[0,244,163,155], regX:81, regY:77, polyArea:[{x:10, y:10}, {x:150, y:10}, {x:150, y:150}, {x:10, y:150}]},
+	{image:"web.png", rect:[242,0,191,181], regX:95, regY:90, polyArea:[{x:10, y:10}, {x:180, y:10}, {x:180, y:180}, {x:10, y:180}]},
+	{image:"web.png", rect:[0,0,242,244], regX:121, regY:122, polyArea:[{x:30, y:30}, {x:210, y:30}, {x:210, y:210}, {x:30, y:210}]}
+]
