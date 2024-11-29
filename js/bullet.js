bb2076da0e5774fec0c4cc57f13dcf21276bb82b
Kiemtien <45493549+vietnamai@users.noreply.github.com>
2024-11-29 09:02:14 +0700

    Update bullet.js

--- js/bullet.js
+++ js/bullet.js
@@ -1,14 +1,3 @@
-const bulletsType = [
-	{image:"bullet.png", rect:[86,0,24,26], regX:12, regY:13},
-	{image:"bullet.png", rect:[61,0,25,29], regX:12, regY:14},
-	{image:"bullet.png", rect:[32,35,27,31], regX:13, regY:15},
-	{image:"bullet.png", rect:[30,82,29,33], regX:14, regY:16},
-	{image:"bullet.png", rect:[0,82,30,34], regX:15, regY:17},
-	{image:"bullet.png", rect:[30,0,31,35], regX:15, regY:17},
-	{image:"bullet.png", rect:[0,44,32,38], regX:16, regY:19}
-];
-
-
 class Bullet {
     constructor(bulletType, startX, startY, targetX, targetY) {
         this.type = bulletType;
