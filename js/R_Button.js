fdef8960721549ba0dbb44f8e74f4b94be8b2a00
Kiemtien <45493549+vietnamai@users.noreply.github.com>
2024-11-30 10:45:57 +0700

    Update js

---
+++ js/R_Button.js
@@ -0,0 +1,3 @@
+// File: js/R_Button.js
+const cannonMinus = {image:"bottom.png", up:{rect:[132,72,44,31]}, down:{rect:[88,72,44,31]}, width:44, height:31};
+const cannonPlus = {image:"bottom.png", up:{rect:[44,72,44,31]}, down:{rect:[0,72,44,31]}, width:44, height:31};
