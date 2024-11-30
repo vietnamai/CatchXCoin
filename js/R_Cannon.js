7432cec84f79bc7249a7dbedd17ee0c81c0df611
Kiemtien <45493549+vietnamai@users.noreply.github.com>
2024-11-30 10:46:38 +0700

    Update R_Cannon.js

--- js/R_Cannon.js
+++ js/R_Cannon.js
@@ -1,8 +1,3 @@
-const cannonMinus = {image:"bottom.png", up:{rect:[132,72,44,31]}, down:{rect:[88,72,44,31]}, width:44, height:31};
-
-const cannonPlus = {image:"bottom.png", up:{rect:[44,72,44,31]}, down:{rect:[0,72,44,31]}, width:44, height:31};
-
-
 const cannonTypes = [
     {
         image: "cannon1.png",
